2f5237c6f8aa05f137e314912d6742d2725c31b4
wayshjn346-coder <wayshjn346@gmail.com>
2026-04-02 23:34:30 +0900

    2주차

--- Basic/hyeonjin/week02/prob02.py
+++ Basic/hyeonjin/week02/prob02.py
@@ -1,15 +1,7 @@
-# BOJ 1330: 두 수 비교하기
-
 import sys
 input = sys.stdin.readline
-
-A, B = map(int, input().split())
-
-if (A > B):
-    print(">")
-
-elif (A < B):
-    print("<")
-    
-else:
-    print("==")
+while True:
+    a = input() 
+    if not a: 
+        break
+    print(a, end="")
