f4fda300013661b0f43e7fb3100cf0cd7b22409c
opal1031 <kimminseo5004@gmail.com>
2026-04-03 12:23:04 +0900

    chore: Comment_Week02

--- Basic/hyeonjin/week02/prob02.py
+++ Basic/hyeonjin/week02/prob02.py
@@ -1,7 +1,17 @@
 import sys
 input = sys.stdin.readline
+
 while True:
     a = input() 
+
     if not a: 
         break
+
     print(a, end="")
+
+'''
+Comment:
+
+EOFError을 except로 처리하는 방법도 있지만, 이렇게 입력이 없을 때 break하는 방법도 있습니다.
+둘 중 어떤 방법이든 상관없습니다.
+'''
--- Basic/hyeonjin/week02/prob04.py
+++ Basic/hyeonjin/week02/prob04.py
@@ -1,7 +1,17 @@
 import sys
 input=sys.stdin.readline
+
 n=int(input())
+
 for i in range(1,n+1):
     for j in range(i):
         print("*",end="")
+
     print()
+
+'''
+Comment:
+
+현재 코드에서 마지막 줄에 print()가 있습니다.
+불필요한 코드이므로 제거하는 것이 좋습니다.
+'''
