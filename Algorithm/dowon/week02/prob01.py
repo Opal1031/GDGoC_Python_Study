00e36b83b94f036ef29146c47f5c92f1be6baf40
opal1031 <kimminseo5004@gmail.com>
2026-04-07 11:40:22 +0900

    chore: Comment_Week02

--- Algorithm/dowon/week02/prob01.py
+++ Algorithm/dowon/week02/prob01.py
@@ -3,10 +3,21 @@ input = sys.stdin.readline
 
 ans=0
 s=set()
+
 n, m=map(int, input().split())
+
 for i in range(n):
     s.add(input().strip())
+
 for i in range(m):
     if input().strip() in s:
         ans+=1
+
 print(ans)
+
+'''
+Comment:
+
+Good!
+dict도 활용해보면 좋을 것 같습니다.
+'''
