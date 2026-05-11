d3832d78aad2bc8254e528acb66b3600745adb12
opal1031 <kimminseo5004@gmail.com>
2026-05-11 22:57:33 +0900

    chore: Comment_Week03

--- Basic/junhee/week03/week04/prob03.py
+++
@@ -1,8 +0,0 @@
-def solution(citations):
-    citations.sort(reverse=True)
-    
-    for i in range(len(citations)):
-        if citations[i] <= i:
-            return i
-    
-    return len(citations)
