e4bb12cb3beb157711ec4c5da9e8b66c5d3ad18c
opal1031 <kimminseo5004@gmail.com>
2026-05-11 22:45:35 +0900

    chore: Comment_Week04

--- Basic/seahun/week04/prob01.py
+++ Basic/seahun/week04/prob01.py
@@ -3,3 +3,10 @@ def solution(array, commands):
     for i, j, k in commands:
         answer.append(sorted(array[i-1:j])[k-1])
     return answer
+
+'''
+Comment:
+
+[언패킹]
+for i, j, k in commands로 직접 언패킹하는 것이 깔끔합니다.
+'''
--- Basic/seahun/week04/prob02.py
+++ Basic/seahun/week04/prob02.py
@@ -6,3 +6,13 @@ def solution(numbers):
     answer = "".join(str_numbers)
     
     return str(int(answer)) if answer != "0" * len(answer) else "0"
+
+'''
+Comment:
+
+[0 처리]
+str(int(answer))로 자동 leading zero 제거.
+
+대안:
+  return str(int(answer)) if int(answer) != 0 else "0"
+'''
