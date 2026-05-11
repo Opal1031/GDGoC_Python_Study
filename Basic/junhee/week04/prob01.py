1b8dd03c3923155fa5a3131510ae5edf31c6e7de
opal1031 <kimminseo5004@gmail.com>
2026-05-11 22:56:26 +0900

    chore: Comment_Week04

--- Basic/junhee/week04/prob01.py
+++ Basic/junhee/week04/prob01.py
@@ -1,2 +1,14 @@
 def solution(array, commands):
     return [sorted(array[i-1:j])[k-1] for i,j,k in commands]
+
+'''
+코멘트:
+
+[최적 스타일]
+리스트 컴프리헨션으로 간결하고 명확합니다.
+한 줄로 모든 로직을 완성했습니다.
+
+[공백 개선]
+i,j,k 뒤에 공백 추가 권장:
+  for i, j, k in commands
+'''
--- Basic/junhee/week04/prob03.py
+++ Basic/junhee/week04/prob03.py
@@ -6,3 +6,18 @@ def solution(citations):
             return i
 
     return len(citations)
+
+'''
+코멘트:
+
+[더 명확한 내림차순 방식]
+citations.sort(reverse=True)
+
+for i in range(len(citations)):
+    h_index = i + 1
+
+    if citations[i] < h_index:
+        return h_index - 1
+
+return len(citations)
+'''
